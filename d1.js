10eeacebd11633ed7eb072b0ff547609c1a8c740
ket00 <keti.papidze17@gmail.com>
2022-10-06 14:13:24 +0400

    dd

---
+++ d1.js
@@ -0,0 +1,3 @@
+//document.querySelector(".box1 h4").innerHTML="Accounting & Tax"
+document.querySelector(".box2 h4").innerHTML="Business Planning"
+document.querySelector(".box3 h4").innerHTML="Employment Issues"
